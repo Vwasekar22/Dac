0933ef3171191577666b3be685cd583248d4760a
Vwasekar22 <vwasekar22@gmail.com>
2019-12-05 06:27:34 -0800

    Calculator commited

---
+++ calculator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float x=20;
+float y=5;
+
+cout<<"The addition of "<<x<<"+"<<y<<"="<<x+y;
+cout<<"The substraction of "<<x<<"-"<<y<<"="<<x-y;
+cout<<"The product of "<<x<<"*"<<y<<"="<<x*y;
+cout<<"The division of "<<x<<"/"<<y<<"="<<x/y;
+return 0;
+}
