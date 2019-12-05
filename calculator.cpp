f120b445750b9689e384424927eb86a6c6e54378
Vwasekar22 <58453391+Vwasekar22@users.noreply.github.com>
2019-12-05 06:54:36 -0800

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -5,9 +5,9 @@ int main()
 float x=20;
 float y=5;
 
-cout<<"The addition of "<<x<<"+"<<y<<"="<<x+y;
-cout<<"The substraction of "<<x<<"-"<<y<<"="<<x-y;
-cout<<"The product of "<<x<<"*"<<y<<"="<<x*y;
-cout<<"The division of "<<x<<"/"<<y<<"="<<x/y;
+cout<<"The addition of "<<x<<"+"<<y<<"="<<x+y<<endl;
+cout<<"The substraction of "<<x<<"-"<<y<<"="<<x-y<<endl;
+cout<<"The product of "<<x<<"*"<<y<<"="<<x*y<<endl;
+cout<<"The division of "<<x<<"/"<<y<<"="<<x/y<<endl;
 return 0;
 }
